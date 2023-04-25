612b56b993e5f01c511b2e8d181efbe1b0f20713
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-26 02:17:04 +0800

    Indention

--- Even and Odd.py
+++ Even and Odd.py
@@ -14,25 +14,24 @@ print("-" * 70)
 #Open numbers.txt (read), even.txt (append), odd.text (append)
 with open("Numbers.txt") as input_data, open("Even.txt", "a") as output_even, open("Odd.txt", "a") as output_odd:
 
-#From the first line up to the end, read numbers.txt 
+    #From the first line up to the end, read numbers.txt 
     for line in input_data:
         input_data = int(line)
 
-#If the number is even
-    if input_data % 2 == 0:
+        #If the number is even
+        if input_data % 2 == 0:
         
-#Calculate for its square
-        square = input_data + input_data
+            #Calculate for its square
+            square = input_data + input_data
         
-#Type it to Even.txt
-        output_even.write(str(square) + "\n")
+            #Type it to Even.txt
+            output_even.write(str(square) + "\n")
 
-# If the number is odd
-    else:
-        print()
+        #If the number is odd
+        else:
 
-#Calculate for its cube
-        cube = input_data + input_data + input_data
+            #Calculate for its cube
+            cube = input_data + input_data + input_data
 
-#Type it to Odd.txt
-        output_odd.write(str(cube) + "\n")
+            #Type it to Odd.txt
+            output_odd.write(str(cube) + "\n")
